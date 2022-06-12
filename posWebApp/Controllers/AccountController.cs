ac815b14944b83ec81ae538fa38a57061d7268a4
git-eng-dina <dinahnemah@gmail.com>
2022-06-12 12:09:52 +0200

    Update AccountController.cs

--- posWebApp/Controllers/AccountController.cs
+++ posWebApp/Controllers/AccountController.cs
@@ -28,7 +28,7 @@ namespace posWebApp.Controllers
             DashBoardModel dashBoardModel = new DashBoardModel();
 
             #region get user from cookie
-            if (Request.Cookies["Cookie1"] != null && Request.Cookies["Cookie1"].Values["UserName"] == userModel.username)
+            if (Request.Cookies["Cookie1"] != null )
             {
                 string name = Request.Cookies["Cookie1"].Values["Image"];
                 Session["UserName"] = Request.Cookies["Cookie1"].Values["UserName"];
